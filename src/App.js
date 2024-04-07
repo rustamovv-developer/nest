88e6556e67010790273496465458b56af3efaecf
--global <--global>
2024-04-07 06:34:59 +0300

    react examen nest website finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/home/Home";
+import Product from "./pages/product/Product";
+import Login from "./pages/login/Login";
+import Auth from "./pages/auth/Auth";
+import Admin from "./pages/admin/Admin";
+import Footer from "./components/footer/Footer";
+import Adv from "./components/adv/Adv";
+import Nav from "./components/nav/Nav";
+import Menu from "./components/menu/Menu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Adv />
+      <Nav />
+      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Auth />}>
+          <Route path="/admin" element={<Admin />} />
+        </Route>
+      </Routes>
+      <Footer />
+      <ToastContainer />
     </div>
   );
 }
